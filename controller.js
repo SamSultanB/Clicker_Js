7f68231e2edd7ca842918fa7311eb8794ed5f386
lux <ssbekmuratov@gmail.com>
2022-10-19 19:04:32 +0600

    second

---
+++ controller.js
@@ -0,0 +1,10 @@
+var counter = 0;
+function count() {
+    counter += 1;
+    document.getElementById("click").innerHTML = counter;
+}
+
+function reset() {
+    counter = 0;
+    document.getElementById("click").innerHTML = counter;
+}
