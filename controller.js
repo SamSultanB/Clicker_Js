8093ac90fb2835d9a09c0c6d21aaddfaefa58b87
lux <ssbekmuratov@gmail.com>
2022-10-27 23:24:03 +0600

    fetching

--- controller.js
+++ controller.js
@@ -1,10 +1,13 @@
-var counter = 0;
-function count() {
-    counter += 1;
-    document.getElementById("click").innerHTML = counter;
-}
+let button = document.getElementById('Play');
+let dolist = document.getElementById('image')
 
-function reset() {
-    counter = 0;
-    document.getElementById("click").innerHTML = counter;
-}
+
+button.addEventListener('click', function() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(res => res.json())
+    .then (result => {
+        console.log(result)
+        dolist.src = result.message
+    })
+    .catch(err => console.log(err))
+})
